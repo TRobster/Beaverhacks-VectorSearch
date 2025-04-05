3ff0d5cc7a4dbf2e5601c3d7c6d5d6c4bd088c26
TRobster <trevorro@uoregon.edu>
2025-04-05 13:43:50 -0700

    Create NeuralNetwork.py

---
+++ NeuralNetwork.py
@@ -0,0 +1,10 @@
+"""
+Author: Trevor Robbins (Back-end)
+Date: 4/5/2025
+Origin: Beaverhacks Hackathon
+Purpose: Create neural network "backbone" for vector search algorithm 
+"""
+
+sentence = "The wide road shimmered in the hot sun"
+tokens = list(sentence.lower().split())
+print(len(tokens))
