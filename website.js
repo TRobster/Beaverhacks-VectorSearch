77068d364ea64a0eb969b09b21d5bc9273a4ecae
Chase Andrew Vanderlip <vanderlc@oregonstate.edu>
2025-04-05 21:17:09 -0700

    Update website.js

--- website.js
+++ website.js
@@ -1,3 +1,7 @@
+
+const button =document.getElementById("myButton");
+
+
 function changeimage(){document.getElementbyId("filter-update-button").addEventListener("click", changeImage);
   
     console.log("tests for enable") 
